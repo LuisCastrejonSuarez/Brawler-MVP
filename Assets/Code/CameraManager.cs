3900f8c34cbc5616a426eca204e6e43fd8b0fdcf
Luis Monster <luis@big-monster.net>
2020-10-08 10:31:52 -0500

    Initial Commit

---
+++ Assets/Code/CameraManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    Camera cam;
+    void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            cam.fieldOfView = 100;
+        }
+        else
+        {
+            cam.fieldOfView = 45;
+        }
+    }
+}
