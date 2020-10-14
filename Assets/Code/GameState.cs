706f25148622459223794e03428910dff0876904
Luis Monster <luis@big-monster.net>
2020-10-14 13:38:51 -0500

    SpriteAtlas

---
+++ Assets/Code/GameState.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    // Inicio Singleton Pattern
+    private static GameState instance;
+    public static GameState Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject go = new GameObject();
+                go.name = typeof(GameState).Name;
+                instance = go.AddComponent<GameState>();
+            }
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        // asigna al nulo
+        if (instance == null)
+        {
+            instance = this as GameState;
+            DontDestroyOnLoad(gameObject);
+        }
+        // destruye al repetido
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/Code/ScreenManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenManager : MonoBehaviour
+{
+    public void GoToScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
