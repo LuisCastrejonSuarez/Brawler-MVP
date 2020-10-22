962d7e80f12861bc97886b220d77842905eded9f
Luis Monster <luis@big-monster.net>
2020-10-22 10:35:52 -0500

    Cinemachine

---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public struct InputMapGame
+{
+    public Vector2 move;
+    public bool punch;
+}
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public InputMapGame input;
+    public void OnMove(InputAction.CallbackContext callback)
+    {
+        input.move = callback.ReadValue<Vector2>();
+    }
+    public void OnPunch(InputAction.CallbackContext callback)
+    {
+        input.punch = true;
+    }
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        Vector3 finalmovement = new Vector3(input.move.x, 0, input.move.y);
+        controller.Move(finalmovement * Time.deltaTime * 5);
+        if (input.punch)
+        {
+            input.punch = false;
+            Debug.Log("punch!");
+        }
+    }
+}
