e430c3a77a27e0c0a8168fb50804827ab5c90d46
Luis Monster <luis@big-monster.net>
2020-11-11 12:17:41 -0600

    Game State - Loader / Writer

--- Assets/Code/GameState.cs
+++ Assets/Code/GameState.cs
@@ -1,11 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+
+[Serializable]
+public class Game
+{
+    public int level;
+    public float timeElapsed;
+    public string playerName;
+}
 
 public class GameState : MonoBehaviour
 {
     // Inicio Singleton Pattern
     private static GameState instance;
+
+    // Save
+    public Game saveData;
+
     public static GameState Instance
     {
         get
@@ -34,5 +48,35 @@ public class GameState : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        StreamReader reader;
+        try
+        {
+            reader = new StreamReader(Application.streamingAssetsPath + "/SaveData.data");
+            while (reader.EndOfStream == false)
+            {
+                string line = reader.ReadLine();
+                instance.saveData = JsonUtility.FromJson<Game>(line);
+                Debug.Log(line);
+                Debug.Log("Level: " + instance.saveData.level);
+            }
+        }catch(System.Exception e)
+        {
+            Save();
+        }
+    }
 
+    public static void Save()
+    {
+        StreamWriter writer;
+
+        instance.saveData.timeElapsed += Time.realtimeSinceStartup;
+
+        string json = JsonUtility.ToJson(instance.saveData);
+
+        writer = new StreamWriter(Application.streamingAssetsPath+"/SaveData.data");
+        writer.Write(json);
+        writer.Close();
+    }
 }
